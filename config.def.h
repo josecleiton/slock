9ace720ff789fa680b3214188786a713d588875c
José Cleiton <jcleitonbc@gmail.com>
2020-07-29 14:59:24 -0300

    slock message

--- config.def.h
+++ config.def.h
@@ -11,6 +11,15 @@ static const char *colorname[NUMCOLS] = {
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
+
 /* time in seconds to cancel lock with mouse movement */
 static const int timetocancel = 4;
 
